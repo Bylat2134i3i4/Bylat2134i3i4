8a546465db051e5a416fd059eafd48187f1ccb27
Bylat Zakirow <Ghost.stop@yandex.ru>
2023-02-06 21:36:00 +0300

    Final without state

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,24 +1,18 @@
 import React, { useState } from 'react';
 import Header from './Components/Header';
 import Body from './Components/Body';
-import Footer from './Components/Footer';
 import LeftBarBlock from './Components/LeftBar';
 import CreateFolderBlock from './Components/CreateFolder';
-import SettBotFold from './Components/SettBotFold';
 import SettMainFold from './Components/SettMainFolder';
 
 const MainPage = () => {
   
   const [LeftBar, ChangeLeftBar] = useState(false);
-  const [BottomBar, ChangeBottomBar] = useState(false);
   const [CreateFolder, ChangeCreateFolder] = useState(false);
   const [MainBar, ChangeMainBar] = useState(false);
 
   const StateLeftBar = () => {
     LeftBar ? ChangeLeftBar(false) : ChangeLeftBar(true) 
-  }
-  const StateBottomBar = () => {
-    BottomBar ? ChangeBottomBar(false) : ChangeBottomBar(true) 
   }
   const StateCreateFolder = () => {
     CreateFolder ? ChangeCreateFolder(false) : ChangeCreateFolder(true) 
@@ -31,11 +25,9 @@ const MainPage = () => {
     <div className='flex flex-col w-full h-full'>
       {LeftBar && <LeftBarBlock />}
       {CreateFolder && <CreateFolderBlock StateCreateFolder={StateCreateFolder}/>}
-      {BottomBar && <SettBotFold />}
       {MainBar && <SettMainFold />}
       <Header StateLeftBar={StateLeftBar} StateCreateFolder={StateCreateFolder}/>
       <Body StateMainBar={StateMainBar}/>
-      <Footer StateBottomBar={StateBottomBar}/>
     </div>
   );
 }
